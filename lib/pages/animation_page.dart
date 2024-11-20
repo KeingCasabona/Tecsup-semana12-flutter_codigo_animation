0e81c4996edbc7dd3366e69e6c510d434a59ba48
KeingCasabona <keing.limache.casabona@gmail.com>
2024-11-19 19:25:18 -0500

    feat: Add animaciones explicitas2

--- lib/pages/animation_page.dart
+++ lib/pages/animation_page.dart
@@ -9,54 +9,74 @@ class AnimationPage extends StatefulWidget {
 
 class _AnimationPageState extends State<AnimationPage>
     with SingleTickerProviderStateMixin {
-  late AnimationController _pruebaAnimationController;
-  late Animation _pruebaAnimation;
+  // late AnimationController _pruebaAnimationController;
+  // late Animation _pruebaAnimation;
+
+  late AnimationController _turnController;
+  late Animation<double> _turnAnimation;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    _pruebaAnimationController = AnimationController(
+    _turnController = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 1200),
+      duration: Duration(seconds: 2),
+    )..repeat(reverse: true);
+    _turnAnimation = CurvedAnimation(
+      parent: _turnController,
+      curve: Curves.easeInOutCirc,
     );
-    _pruebaAnimation = ColorTween(
-      begin: Colors.red,
-      end: Colors.blue,
-    ).animate(_pruebaAnimationController);
+    // _turnController.forward();
 
-    _pruebaAnimationController.forward();
+    // _pruebaAnimationController = AnimationController(
+    //   vsync: this,
+    //   duration: Duration(milliseconds: 1200),
+    // );
+    // _pruebaAnimation = ColorTween(
+    //   begin: Colors.red,
+    //   end: Colors.blue,
+    // ).animate(_pruebaAnimationController);
 
-    _pruebaAnimationController.addListener(() {
-      setState(() {});
-      //print(_pruebaAnimationController.value);
-      print(_pruebaAnimation.value);
-    });
+    // _pruebaAnimationController.forward();
+
+    // _pruebaAnimationController.addListener(() {
+    //   setState(() {});
+    //   //print(_pruebaAnimationController.value);
+    //   print(_pruebaAnimation.value);
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AnimationApp'),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-            animation: _pruebaAnimation,
-            builder: (BuildContext context, _) {
-              return IconButton(
-                onPressed: () {
-                  //_pruebaAnimationController.forward();
-                },
-                icon: Icon(
-                  Icons.ac_unit,
-                  size: 200,
-                ),
-                color: _pruebaAnimation.value,
-              );
-            }),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('AnimationApp'),
+        ),
+        body: Center(
+          child: RotationTransition(
+              turns: _turnAnimation,
+              child: FlutterLogo(
+                size: 40,
+              )),
+        )
+        // body: Center(
+        //   child: AnimatedBuilder(
+        //       animation: _pruebaAnimation,
+        //       builder: (BuildContext context, _) {
+        //         return IconButton(
+        //           onPressed: () {
+        //             //_pruebaAnimationController.forward();
+        //           },
+        //           icon: Icon(
+        //             Icons.ac_unit,
+        //             size: 200,
+        //           ),
+        //           color: _pruebaAnimation.value,
+        //         );
+        //       }),
+        // ),
+        );
   }
 }
